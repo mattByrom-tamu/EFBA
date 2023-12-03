c19a97308494ab58ae8de8972e3dacdb5a6698ca
Matthew Byrom <matthew.james.byrom@tamu.edu>
2023-12-03 09:35:10 -0500

    Remade EBAtimeseries package in this repo. Removed a lot fo excess files. Set up with same packageManagement Script (authors, liscense, dependencies EBA, library install.

---
+++ R/packageManagementScript.R
@@ -0,0 +1,71 @@
+# package management script
+
+# install R packages for packaging
+# install.packages(c("devtools", "usethis", "roxygen2"))
+library(usethis)
+library(devtools)
+library(roxygen2)
+library(desc)
+
+# install.packages(c("momentchi2", "fields", "viridis", "signal", "fossil"))
+library(momentchi2)
+library(fields)
+library(viridis)
+library(signal)
+library(fossil)
+
+# install.packages(c("ggplot2"))
+library(ggplot2)
+
+# ignored files
+use_build_ignore(c("packageManagementScript.R", "EBA-master", "fEBA-main", "mEBA-main"))
+
+# add author and manager
+
+# add Dr. Bruce
+use_author(
+  given = "Scott",
+  family = "Bruce",
+  role = c("aut", "cre"),
+  email = "sabruce@tamu.edu"
+)
+
+# add Matt Byrom
+use_author(
+  given = "Matt",
+  family = "Byrom",
+  role = c("cre"),
+  email = "matthew.james.byrom@tamu.edu"
+)
+
+# add Dylan Ward
+use_author(
+  given = "Dylan",
+  family = "Ward",
+  role = c("cre"),
+)
+
+# add Mohit Chhaparia
+use_author(
+  given = "Mohit",
+  family = "Chhaparia",
+  role = c("cre"),
+)
+
+# add GPL 3 liscense
+usethis::use_gpl3_license()
+usethis::use_mit_license()
+
+# create cpp infrastructure
+
+
+
+
+# package dependencies for EBA
+usethis::use_package("momentchi2", type = "Imports")
+usethis::use_package("fields", type = "Imports")
+usethis::use_package("viridis", type = "Imports")
+usethis::use_package("signal", type = "Imports")
+usethis::use_package("fossil", type = "Imports")
+
+devtools::install_github("mattByrom-tamu/EBAtimeseries")
